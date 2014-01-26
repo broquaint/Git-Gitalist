50411d6de5aedf3731751e1aa270b5c6714f3fca
Dan Brook <dan@broquaint.com>
2014-01-26 14:30:32 +0100

    Convert Util classes to M::S
    Which was the last of the library code to have MX::D to be excised. Now
    to root it out entirely.

--- lib/Git/Gitalist/HasUtils.pm
+++ lib/Git/Gitalist/HasUtils.pm
@@ -1,30 +1,34 @@
-use MooseX::Declare;
+package Git::Gitalist::HasUtils;
 
-role Git::Gitalist::HasUtils {
-    use Git::Gitalist::Util;
+use Moose::Role;
 
-    method BUILD {}
-    after BUILD {
-        # Force value build
-        $self->meta->get_attribute('_util')->get_read_method_ref->($self);
-    }
+use Method::Signatures;
 
-    has _util => (
-        isa => 'Git::Gitalist::Util',
-        lazy => 1,
-        is => 'bare',
-        builder => '_build_util',
-        handles => [qw/
-            run_cmd
-            run_cmd_fh
-            run_cmd_list
-            get_gpp_object
-            gpp
-        /],
-        traits => ['DoNotSerialize']
-    );
-    method _build_util { confess(shift() . " cannot build _util") }
-}
+use Git::Gitalist::Util;
+
+method BUILD {}
+
+after BUILD => method(...) {
+  # Force value build
+  $self->meta->get_attribute('_util')->get_read_method_ref->($self);
+};
+
+has _util => (
+  isa => 'Git::Gitalist::Util',
+  lazy => 1,
+  is => 'bare',
+  builder => '_build_util',
+  handles => [qw(
+    run_cmd
+    run_cmd_fh
+    run_cmd_list
+    get_gpp_object
+    gpp
+  )],
+  traits => ['DoNotSerialize']
+);
+
+method _build_util { confess(shift() . " cannot build _util") }
 
 1;
 
--- lib/Git/Gitalist/Util.pm
+++ lib/Git/Gitalist/Util.pm
@@ -1,75 +1,84 @@
-use MooseX::Declare;
+package Git::Gitalist::Util;
 
-class Git::Gitalist::Util {
-    use File::Which;
-    use Git::PurePerl;
-    use IPC::Run qw(run start);
-    use Symbol qw(geniosym);
-    use MooseX::Types::Common::String qw/NonEmptySimpleStr/;
+use Moose;
 
-    has repository => (
-        isa => 'Git::Gitalist::Repository',
-        handles => { gitdir => 'path' },
-        is => 'bare', # No accessor
-        weak_ref => 1, # Weak, you have to hold onto me.
-        predicate => 'has_repository',
-    );
-    has _git      => ( isa => NonEmptySimpleStr, is => 'ro', lazy_build => 1 );
-    sub _build__git {
-        my $git = File::Which::which('git');
+use Method::Signatures;
 
-        if (!$git) {
-            die <<EOR;
+use File::Which;
+use Git::PurePerl;
+use IPC::Run qw(run start);
+use Symbol qw(geniosym);
+use MooseX::Types::Common::String qw/NonEmptySimpleStr/;
+
+has repository => (
+  isa       => 'Git::Gitalist::Repository',
+  handles   => { gitdir => 'path' },
+  is        => 'bare',                 # No accessor
+  weak_ref  => 1,                      # Weak, you have to hold onto me.
+  predicate => 'has_repository',
+);
+
+has _git => (
+  isa        => NonEmptySimpleStr,
+  is         => 'ro',
+  lazy_build => 1,
+);
+
+has gpp => (
+  isa => 'Git::PurePerl', is => 'ro', lazy => 1,
+  default => sub {
+    my $self = shift;
+    confess("Cannot get gpp without repository")
+         unless $self->has_repository;
+    Git::PurePerl->new(gitdir => $self->gitdir);
+  },
+);
+
+method _build__git {
+  my $git = File::Which::which('git');
+
+  if (!$git) {
+    die <<EOR;
 Could not find a git executable.
 Please specify the which git executable to use in gitweb.yml
 EOR
-        }
+  }
 
-        return $git;
-    }
+  return $git;
+}
 
-    has gpp      => (
-        isa => 'Git::PurePerl', is => 'ro', lazy => 1,
-        default => sub {
-            my $self = shift;
-            confess("Cannot get gpp without repository")
-                unless $self->has_repository;
-            Git::PurePerl->new(gitdir => $self->gitdir);
-        },
-    );
+method run_cmd (@args) {
+  unshift @args, ( '--git-dir' => $self->gitdir )
+       if $self->has_repository;
+  #        print STDERR 'RUNNING: ', $self->_git, qq[ @args], $/;
+  run [$self->_git, @args], \my($in, $out, $err);
 
-    method run_cmd (@args) {
-        unshift @args, ( '--git-dir' => $self->gitdir )
-            if $self->has_repository;
-#        print STDERR 'RUNNING: ', $self->_git, qq[ @args], $/;
-        run [$self->_git, @args], \my($in, $out, $err);
+  return $out;
+}
 
-        return $out;
-    }
+method run_cmd_fh (@args) {
+  my ($in, $out, $err) = (geniosym, geniosym, geniosym);
+  unshift @args, ('--git-dir' => $self->gitdir)
+       if $self->has_repository;
+  #        print STDERR 'RUNNING: ', $self->_git, qq[ @args], $/;
+  start [$self->_git, @args],
+       '<pipe', $in,
+       '>pipe', $out,
+       '2>pipe', $err
+            or die "cmd returned *?";
+  return $out;
+}
 
-    method run_cmd_fh (@args) {
-        my ($in, $out, $err) = (geniosym, geniosym, geniosym);
-        unshift @args, ('--git-dir' => $self->gitdir)
-            if $self->has_repository;
-#        print STDERR 'RUNNING: ', $self->_git, qq[ @args], $/;
-        start [$self->_git, @args],
-            '<pipe', $in,
-            '>pipe', $out,
-            '2>pipe', $err
-                or die "cmd returned *?";
-        return $out;
-    }
+method run_cmd_list (@args) {
+  my $cmdout = $self->run_cmd(@args);
+  return $cmdout ? split(/\n/, $cmdout) : ();
+}
 
-    method run_cmd_list (@args) {
-        my $cmdout = $self->run_cmd(@args);
-        return $cmdout ? split(/\n/, $cmdout) : ();
-    }
+method get_gpp_object (MooseX::Types::Common::String::NonEmptySimpleStr $sha1) {
+  return $self->gpp->get_object($sha1) || undef;
+}
 
-    method get_gpp_object (NonEmptySimpleStr $sha1) {
-        return $self->gpp->get_object($sha1) || undef;
-    }
-
-} # end class
+1;
 
 __END__
 

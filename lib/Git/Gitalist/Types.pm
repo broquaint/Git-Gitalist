1d2c6802495a70490ad88e363c03da01acbac8bc
Dan Brook <dan@broquaint.com>
2014-02-01 17:41:52 +0100

    Drop unused types
    The Gitalist specific types are no longer of use and the custom Dir just
    went by the wayside.

--- lib/Git/Gitalist/Types.pm
+++ lib/Git/Gitalist/Types.pm
@@ -1,19 +1,12 @@
 package Git::Gitalist::Types;
 
-use MooseX::Types
-     -declare => [qw/
-         SHA1
-         Dir
-         ArrayRefOfDirs
-         DirOrUndef
-     /];
+use MooseX::Types -declare => [qw/ SHA1 /];
 
-use MooseX::Types::Path::Class;
-use MooseX::Types::DateTime qw/ DateTime /;
 use MooseX::Storage::Engine ();
-use MooseX::Types::Common::String qw/NonEmptySimpleStr/;
-use MooseX::Types::Moose qw/ ArrayRef Undef Str /;
-use Path::Class qw/ dir /;
+
+use MooseX::Types::DateTime qw/ DateTime /;
+use MooseX::Types::Common::String qw/ NonEmptySimpleStr /;
+use MooseX::Types::Moose qw/ ArrayRef /;
 
 subtype SHA1,
     as NonEmptySimpleStr,
@@ -32,28 +25,6 @@ MooseX::Storage::Engine->add_custom_type_handler(
         },
         collapse => sub {
             $_[0]->ymd('-') . 'T' . $_[0]->hms(':') . 'Z' 
-        },
-);
-
-subtype Dir,
-    as 'MooseX::Types::Path::Class::Dir',
-    where { 1 };
-
-subtype ArrayRefOfDirs, as ArrayRef[Dir], where { scalar(@$_) >= 1 }, message { "Cannot find repository dir" };
-coerce ArrayRefOfDirs, from NonEmptySimpleStr, via { [ dir($_)->resolve ] };
-coerce ArrayRefOfDirs, from ArrayRef[NonEmptySimpleStr], via { [ map { dir($_)->resolve } @$_ ] };
-
-subtype DirOrUndef, as Dir | Undef;
-coerce DirOrUndef, from Str, via { if ($_) { dir($_) } else { undef }};
-
-MooseX::Storage::Engine->add_custom_type_handler(
-    Dir,
-        expand   => sub {
-            my $val = shift;
-            Carp::confess("Not implemented");
-        },
-        collapse => sub {
-            shift() . '';
         },
 );
 
